27e849ed92f6f1664595bfbd2226da378f90116c
orbtl <twinsendude@gmail.com>
2020-02-05 14:09:37 -0800

    refactoring dictionary into static class

--- evilHangman/Computer.cs
+++ evilHangman/Computer.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 public class Computer
 {
     public Player User;
     public int wordLength;
+    public List<string> currentDict;
     public void startGame(){
         System.Console.WriteLine("Welcome to Evil Hangman!  Are you prepared to face your certain demise!?");
         System.Console.WriteLine("Please enter your name");
@@ -13,24 +15,24 @@ public class Computer
     }
     public void calcDifficulty(){
         System.Console.WriteLine(@"Please choose a difficulty:
-        (1) Easy: 1-7 Letters
-        (2) Medium: 8-15 Letters
-        (3) Hard: 16-45 Letters");
+        (1) Easy: 2-7 Letters
+        (2) Medium: 8-13 Letters
+        (3) Hard: 14-22 Letters");
         string input = Console.ReadLine();
         Random rand = new Random();
         int min;
         int max;
         if (input == "1"){
-            min = 1;
+            min = 2;
             max = 8;
         }
         else if (input == "2") {
             min = 8;
-            max = 16;
+            max = 14;
         }
         else if (input == "3") {
-            min = 16;
-            max = 46;
+            min = 14;
+            max = 23;
         }
         else {
             System.Console.WriteLine("That is not a valid input, please enter 1, 2, or 3");
--- evilHangman/dictionary/Dictionary.cs
+++ evilHangman/dictionary/Dictionary.cs
@@ -1,22 +1,17 @@
 using System.Collections.Generic;
 using System.IO;
-public class wordDict {
-    public List<string>[] allWords;
-    public wordDict(){
-        this.allWords = new List<string>[23];
-    }
-    
-    public void generate(){
+public static class wordDict {
+    public static List<string> generate(int targetLength){
         int wordLength;
-        for (int i=0; i<=22; i++) {
-            this.allWords[i] = new List<string>();
-        }
+        List<string> allWords = new List<string>();
+    
         foreach (string line in File.ReadLines(@"./dictionary/dictionary.txt")){
             wordLength = line.Length;
-            if (wordLength <= 22) {
-                this.allWords[wordLength].Add(line);
+            if (wordLength == targetLength) {
+                allWords.Add(line);
             }
         }
+        return allWords;
     }
 
 }
