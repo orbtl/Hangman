fe8473c802172bda7cb6f22ef4fae2b8f0a6dfad
orbtl <twinsendude@gmail.com>
2020-02-05 15:16:08 -0800

    Built out working game logic

--- evilHangman/Computer.cs
+++ evilHangman/Computer.cs
@@ -47,12 +47,13 @@ public class Computer
             this.wordStatus[i] = "_";
         }
         this.currentDict = wordDict.generate(this.wordLength);
-        System.Console.WriteLine($"Playing with a word of length {this.wordLength}");
-        System.Console.WriteLine($"First word in Dict {this.currentDict[0]}");
+        // System.Console.WriteLine($"Playing with a word of length {this.wordLength}");
+        // System.Console.WriteLine($"First word in Dict {this.currentDict[0]}");
+        this.displayTurn();
         return;
     }
 
-    public void displayTurn(){
+    public void displayTurn(string msg=""){
         Console.Clear();
         System.Console.WriteLine($"***************** Letters you can still choose ***********************");
 
@@ -64,6 +65,85 @@ public class Computer
         System.Console.Write("                  ");
         foreach(string place in this.wordStatus){
             System.Console.Write($"{place} ");
+        }
+        System.Console.WriteLine("");
+        System.Console.WriteLine($"You have {this.User.Guesses} guesses remaining...");
+        System.Console.WriteLine(msg);
+        this.calcTurn();
+    }
+
+    public void calcTurn(){
+        System.Console.WriteLine("Please enter a letter to guess:");
+        string inputGuess = Console.ReadLine();
+        int availIndex = this.User.AvailLetters.IndexOf(inputGuess);
+        if (availIndex == -1 || inputGuess == "-"){
+            System.Console.WriteLine("Sorry that's not a valid letter to guess");
+            this.calcTurn();
+            return;
+        }
+
+        this.User.AvailLetters[availIndex] = "-";
+        this.determineWord(inputGuess);
+    }
+
+    public void determineWord(string letter){
+        int indexFound;
+        List<List<string>> wordGroups = new List<List<string>>();
+        for(int j=0; j <= this.currentDict[0].Length; j++){
+            wordGroups.Add(new List<string>());
+        }
+        foreach( string word in this.currentDict){
+            indexFound = word.Length;
+            for(int i=0; i<word.Length; i++){
+                if(word[i].ToString() == letter){
+                    if(indexFound != word.Length){
+                        indexFound = -1;
+                    }
+                    else{
+                        indexFound = i;
+                    }
+                }
+            }
+            if(indexFound != -1){
+                wordGroups[indexFound].Add(word);
+            }
+        }
+        int maxLength = wordGroups[0].Count;
+        int correctIndex = 0;
+        for( int k =1; k < wordGroups.Count; k++){
+            if(wordGroups[k].Count > maxLength){
+                maxLength = wordGroups[k].Count;
+                correctIndex = k;
+            }
+        }
+        this.currentDict = wordGroups[correctIndex];
+        if (correctIndex >= this.wordLength) {
+            this.User.Guesses --;
+            if (this.User.Guesses == 0){
+                this.gameOver();
+                return;
+            }
+            else {
+            this.displayTurn("Incorrect Guess!  Try again, LOSER!");
+            }
+        }
+        else {
+            this.wordStatus[correctIndex] = letter;
+            this.displayTurn();
+        }
+        return;
+    }
+    public void gameOver(){
+        System.Console.WriteLine("GAME OVER!!!! YOU LOSE!!!!");
+        System.Console.WriteLine($"The word may or may not have been '{this.currentDict[0]}'");
+        System.Console.WriteLine("Would you like to play again? (y/n)");
+        string input = Console.ReadLine().ToLower();
+        if (input == "y") {
+            this.startGame();
+            return;
+        }
+        else {
+            return;
         }
     }
 }
